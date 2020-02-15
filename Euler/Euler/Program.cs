c3fe8ecb29278ecca5f306a2c3a93ccf0f782725
Rody10 <rodneysimbatholanah@gmail.com>
2020-02-15 13:36:22 +0200

    completed Euler problem 2

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -6,7 +6,42 @@ namespace Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void EulerProblem2()
+            {
+                const int max = 4000000;
+                int evenSum = 2;
+                
+                
+                int varFirst = 1;
+                int varSecond = 2;
+                while (true)
+                {
+                    int nxt = varFirst + varSecond;
+                    if (nxt > max)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (nxt%2 == 0)
+                        {
+                            evenSum = evenSum + nxt;
+                        }
+                        varFirst = varSecond;
+                        varSecond = nxt;
+
+                    }
+                }
+                Console.WriteLine("Sum of even fibonacci numbers less than 4 million is "+evenSum);
+                Console.Read();
+                       
+            }
+            EulerProblem2();
+
+            
         }
+
+        
     }
+    
 }
