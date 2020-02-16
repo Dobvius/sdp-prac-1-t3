bc38ece7e55590ad8ede98afe5bfe4c0b96a9694
Th3Doo <scottiec4rock@gmail.com>
2020-02-16 21:12:05 +0200

    Completed Euler Problem 6

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -6,7 +6,29 @@ namespace Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            void EulerProblem6()
+            {
+                int num = 1;
+                int sumOfSq = 0;
+                int sqOfSum = 0;
+
+                while (num <= 100)
+                {
+                    sumOfSq += num * num;
+                    sqOfSum += num;
+                    num++;
+                }
+
+                sqOfSum = sqOfSum * sqOfSum;
+                Console.WriteLine("The sum of the squares of the first 100 natural numbers is " + sumOfSq.ToString());
+                Console.WriteLine("The square of the sum of the first 100 natural numbers is " + sqOfSum.ToString());
+                Console.WriteLine("The difference is " + (sqOfSum - sumOfSq).ToString());
+                Console.Read();
+            }
+            EulerProblem6();
         }
+
     }
+
+
 }
